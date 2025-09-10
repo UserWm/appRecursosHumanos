68321ec3981bcbd16f60cbbe4e76f6d4b1d50de6
William Morales <j.william03@hotmail.com>
2025-09-10 10:25:32 -0600

    agregado

--- personas.php
+++ personas.php
@@ -1,3 +1,8 @@
+<?php
+
+include_once('./conf/conf.php');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,21 +12,60 @@
 </head>
 <body>
     <form action="" method="POST" enctype="multipart/form-data">
-    <input type="file" name="foto">
+    <input type="file" name="foto" >
     <input type="submit" value="enviar">
     </form>
+
+        <table>
+            <tr>
+            <th>archivos</th>
+            </tr>
+            
+                <?php 
+                $consultarfotos="SELECT * FROM tbl_personas";
+                $consulta=mysqli_query($con,$consultarfotos);
+
+                while($data=mysqli_fetch_array($consulta)){
+                $info= new SplFileInfo($data['foto']);
+                $extension = pathinfo($info->getFilename(), PATHINFO_EXTENSION);
+                echo "<tr>";
+                if($extension== "png" ){
+                echo "<td> <img style='width:100px;'src='./imgs/".$data['foto']."'></td>";
+                }else{
+                    echo "<td><a href='./imgs/".$data['foto']."' target='_blank'> Ver</a></td>";
+                }
+                echo "</tr>";
+                }
+                ?>
+            
+        </table>
 </body>
 </html>
 <?php
-include_once('./conf/conf.php');
+
 if ($_SERVER["REQUEST_METHOD"] === "POST"){
     $imagen = isset($_FILES['foto']) ? $_FILES['foto']:"";
 
-    $nombre= $_FILES['foto']['name'];
-    $temnombre= $_FILES['foto']['tmp_name'];
-    echo $nombre;
+    // var_dump($imagen);
+    $tamano= $_FILES['foto']['size'];
+   $temname=$_FILES['foto']['tmp_name'];
+   $nombre=$_FILES['foto']['name'];
+    // $valorMb= $tamano/1048576;
+    // echo "Tamaño ".$valorMb." MB <br>";
+    // echo "temporal name ".$temname."<br>";
+    // echo "Nombre ". $nombre."<br>";
 
-    move_uploaded_file($temnombre, './imgs/'.$nombre);
+    // $nombre= $_FILES['foto']['name'];
+    // $temnombre= $_FILES['foto']['tmp_name'];
+    // echo $nombre;
+
+    $ruta= "./imgs/".$nombre;
+    if(move_uploaded_file($temname, './imgs/'.$nombre))
+    {
+     $consulta="INSERT INTO tbl_personas (id, foto) values (null, '$nombre')";
+     $ejecutar= mysqli_query($con,$consulta);
+    }
+
 }
 
 ?>
